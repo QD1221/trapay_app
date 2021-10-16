20ff2b98e8952c7c4aeb304c53557826f3dab41c
QD1221 <zxcvbnm12122000@gmail.com>
2021-10-16 08:11:12 +0700

    Initial commit

---
+++ android/app/src/main/kotlin/com/qd/trapay_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.qd.trapay_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/src/screen/home_screen.dart
@@ -0,0 +1,183 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int bottomTabIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: IndexedStack(
+        index: bottomTabIndex,
+        children: [
+          SafeArea(
+            child: SingleChildScrollView(
+              child: Padding(
+                padding: const EdgeInsets.only(left: 12, top: 12, bottom: 0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(right: 12),
+                      child: Row(
+                        children: [
+                          Container(
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(8),
+                                color: Colors.yellow),
+                            padding: EdgeInsets.all(12),
+                            child: Icon(Icons.qr_code),
+                          ),
+                          SizedBox(width: 12),
+                          Text(
+                            'Scan To Pay',
+                            style: TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          Spacer(),
+                          CircleAvatar()
+                        ],
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 16),
+                      child: SizedBox(
+                        height: 160,
+                        child: Container(
+                          color: Colors.blue,
+                        ),
+                      ),
+                    ),
+                    Row(
+                      children: [
+                        Text(
+                          'Services',
+                          style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Spacer(),
+                        TextButton(
+                          onPressed: () {},
+                          child: Text('See More'),
+                          style: TextButton.styleFrom(primary: Colors.grey),
+                        )
+                      ],
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 8),
+                      child: SizedBox(
+                        height: 72,
+                        child: Container(
+                          color: Colors.blue,
+                        ),
+                      ),
+                    ),
+                    Row(
+                      children: [
+                        Text(
+                          'History',
+                          style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Spacer(),
+                        IconButton(
+                          onPressed: () {},
+                          icon: Icon(Icons.filter_list),
+                          color: Colors.grey,
+                        )
+                      ],
+                    ),
+                    Text('Last 10 Days'),
+                    Padding(
+                      padding: const EdgeInsets.only(right: 12),
+                      child: Column(
+                        children: List.generate(
+                          10,
+                          (index) => Padding(
+                            padding: const EdgeInsets.symmetric(vertical: 8),
+                            child: SizedBox(
+                              height: 64,
+                              child: Container(color: Colors.grey),
+                            ),
+                          ),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ),
+          Container(
+            child: Center(
+              child: Text('Services'),
+            ),
+          ),
+          Container(
+            child: Center(
+              child: Text('Chatting'),
+            ),
+          ),
+          Container(
+            child: Center(
+              child: Text('Groups'),
+            ),
+          ),
+          Container(
+            child: Center(
+              child: Text('More'),
+            ),
+          ),
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: bottomTabIndex,
+        type: BottomNavigationBarType.fixed,
+        selectedLabelStyle: TextStyle(fontSize: 10),
+        selectedFontSize: 10,
+        unselectedLabelStyle: TextStyle(fontSize: 10),
+        onTap: (idx) {
+          setState(() {
+            bottomTabIndex = idx;
+          });
+        },
+        selectedItemColor: Colors.black,
+        unselectedItemColor: Colors.grey,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.apps),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.layers),
+            label: 'Services',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.message),
+            label: 'Chatting',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.supervisor_account),
+            label: 'Groups',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.more_horiz),
+            label: 'More',
+          )
+        ],
+      ),
+    );
+  }
+}
